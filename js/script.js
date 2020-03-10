771505d065d63e062ffbe626b23ce744a18de7ec
Gongmeda <gongmeda@gmail.com>
2020-03-10 19:40:23 +0900

    Implement lists
    - implement list modal
    - implement list object array
    - implement add list
    - implement delete list

--- js/script.js
+++ js/script.js
@@ -45,6 +45,9 @@ add_btn_click();
 //// modal fundtions
 let modal = document.querySelector('.modal');
 let modal_content = document.querySelector('.modal_content');
+let modal_header = document.querySelector('.modal_header');
+let modal_body = document.querySelector('.modal_body');
+let modal_footer = document.querySelector('.modal_footer');
 // modal open button
 function modal_open(btn, set_modal) {
     btn.onclick = function () {
@@ -78,15 +81,86 @@ window.onclick = function (event) {
     }
 }
 
+//// add / delete list-element function
+let lists = document.querySelector('#lists');
+let add_list_btn = document.querySelector('#add_list');
+// list-element object / array
+let list_element = function (list_text, list_state) {
+    this.list_text = list_text;
+    this.list_state = list_state;
+}
+let list_array = [];
+// push examples in the array
+list_array.push(new list_element('웹프로그래밍 공부', 'done'));
+list_array.push(new list_element('헬스', 'ongoing'));
+list_array.push(new list_element('영화 보기', 'undone'));
+// show all list-elements in the array
+function list_reload() {
+    list_delete_all();
+    list_array.forEach(function(element) {
+        let el = document.createElement('li');
+        el.classList.add("list_element", element.list_state);
+        // set text
+        let el_text = document.createTextNode(element.list_text);
+        el.appendChild(el_text);
+        // set button
+        let btn = document.createElement('button');
+        btn.classList.add("btn", "edit_btn");
+        if (element.list_state == 'undone') {
+            btn.innerHTML = '<i class="fas fa-times"></i>'
+        } else if (element.list_state == 'ongoing') {
+            btn.innerHTML = '<i class="fas fa-long-arrow-alt-right"></i>'
+        } else if (element.list_state == 'done') {
+            btn.innerHTML = '<i class="fas fa-check"></i>'
+        } else {
+            console.log('error in the list_array')
+        }
+        el.appendChild(btn);
+
+        lists.appendChild(el);
+    })
+}
+// initial reload function execution
+window.addEventListener('DOMContentLoaded', function() {
+    list_reload();
+})
+// set modal to list and open modal
+function list_modal_open() {
+    modal_header.firstElementChild.innerText = 'To-do';
+    modal_body.innerHTML = '<input type="text" name="" id="list_area" placeholder="write To-do here.">';
+    modal_footer.innerHTML = '<i class="modal_icon list_delete fas fa-trash-alt fa-xs"></i><i class="modal_icon list_edit far fa-check-square fa-sm"></i>'
+    // delete list
+    document.querySelector('.list_delete').onclick = function () {
+        modal_close();
+        list_array.length = 0;
+        list_reload();
+    }
+    // edit list
+    document.querySelector('.list_edit').onclick = function () {
+        if (list_area.value == '') {
+            window.alert('you have to input text!');
+        } else {
+            modal_close();
+            list_array.push(new list_element(list_area.value, 'undone'));
+        }
+        list_reload();
+    }
+}
+modal_open(add_list_btn, list_modal_open);
+// delete all element
+function list_delete_all() {
+    while (lists.firstChild) {
+        lists.removeChild(lists.firstChild);
+    }
+}
+
 //// add / delete memo function
 let memo = document.querySelector('#memo');
 let add_memo_btn = document.querySelector('#add_memo');
-let modal_header = document.querySelector('.modal_header');
-let modal_body = document.querySelector('.modal_body');
-let modal_footer = document.querySelector('.modal_footer');
+
 // set modal to memo and open modal
 function memo_modal_open() {
-    modal_header.childNodes[1].innerText = 'memo';
+    modal_header.firstElementChild.innerText = 'memo';
     modal_body.innerHTML = '<textarea name="memo" id="memo_area" cols="30" rows="8" placeholder="write memo here."></textarea>';
     modal_footer.innerHTML = '<i class="modal_icon memo_delete fas fa-trash-alt fa-xs"></i><i class="modal_icon memo_edit far fa-check-square fa-sm"></i>'
     // keep original memo in the textarea
